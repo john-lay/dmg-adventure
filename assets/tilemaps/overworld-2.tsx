f44284fd2c1c48a88ad82134401d02436c143b15
john-lay <john_lay@hotmail.co.uk>
2020-04-19 13:54:08 +0100

    Generate a tilemap from tilesets
    Created using tiled.

---
+++ assets/tilemaps/overworld-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="overworld-2" tilewidth="8" tileheight="8" tilecount="576" columns="16">
+ <image source="../tilesets/overworld_2.dmg.png" width="128" height="288"/>
+</tileset>
