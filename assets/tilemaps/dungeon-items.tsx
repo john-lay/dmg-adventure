e53af4e9b856ae90be19a9e314bf944046dc68d7
john-lay <john_lay@hotmail.co.uk>
2020-05-01 11:54:58 +0100

    Bake hearts indicator into background
    Attempt to free up space for more sprites by removing extra heart frame.
    
    Also includes rupee indicator

---
+++ assets/tilemaps/dungeon-items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="dungeon-items" tilewidth="8" tileheight="8" tilecount="16" columns="16">
+ <image source="../tilesets/dungeon_items.dmg.png" width="128" height="8"/>
+</tileset>
