767d81f35b427fca0fcc1bd757f6ca1e213a2946
john-lay <john_lay@hotmail.co.uk>
2020-05-24 10:47:52 +0100

    Create title tilemap

---
+++ assets/tilemaps/title-tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="title-tiles" tilewidth="8" tileheight="8" tilecount="112" columns="16">
+ <image source="../tilesets/title.png" width="128" height="56"/>
+</tileset>
