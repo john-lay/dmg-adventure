d4bc7d9cc87cb8c457abbd20ace1f4c65e0065ff
john-lay <john_lay@hotmail.co.uk>
2020-05-24 11:35:59 +0100

    Prototype game over screen with quest status

---
+++ assets/tilemaps/quest-items.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="quest-items" tilewidth="8" tileheight="8" tilecount="16" columns="8">
+ <image source="../tilesets/quest-items.png" width="64" height="16"/>
+</tileset>
