50587c89f9dbab6a0316a099f974e37a1eeb14eb
john-lay <john_lay@hotmail.co.uk>
2020-05-27 18:04:05 +0100

    Replace custom tileset with disassembled tileset

---
+++ assets/tilemaps/dungeons-2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="dungeons-2" tilewidth="8" tileheight="8" tilecount="256" columns="2">
+ <image source="../tilesets/dungeons_2.dmg.png" width="16" height="1024"/>
+</tileset>
