f8d5b20073344b44bf05c3bcc1eeeae93a4cabaf
john-lay <john_lay@hotmail.co.uk>
2020-06-04 17:39:36 +0100

    Add wall and floor tiles for cave interior

---
+++ assets/tilemaps/floor-b.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.2" name="floor-b" tilewidth="8" tileheight="8" tilecount="16" columns="2">
+ <image source="../tilesets/floor_b.dmg.png" width="16" height="64"/>
+</tileset>
