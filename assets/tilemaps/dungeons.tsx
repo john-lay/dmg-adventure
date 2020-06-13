0a2e3ab57e2d4a36db2a3ca1cabd527977be5587
john-lay <john_lay@hotmail.co.uk>
2020-06-13 19:10:41 +0100

    Delete redundant dungeons master tilemap

--- assets/tilemaps/dungeons.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.2" name="dungeons" tilewidth="8" tileheight="8" tilecount="1144" columns="22">
- <image source="../tilesets/dungeons.png" width="176" height="416"/>
-</tileset>
